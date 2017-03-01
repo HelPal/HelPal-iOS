c949132844b13d92cd13bf2429481f961b24753a
钟典@Runga <ceo@myaiol.com>
2017-03-01 13:03:54 +0800

    Init

---
+++ HelPal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelPal
+//
+//  Created by Dian Zhong on 2017/3/1.
+//  Copyright © 2017年 Infomation School of Renmin University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
