0404ba26e455eaec3281a63157456ea90e86c7e4
钟典@Runga <ceo@myaiol.com>
2017-03-11 18:09:43 +0800

    Using Container View, Segmented Controller to switch View Controllers,

--- HelPal/Extensions/String.swift
+++ HelPal/Extensions/String.swift
@@ -12,4 +12,9 @@ extension String {
     var localized: String {
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
     }
+    
+    //Try not to use this, avoiding complex this app.
+    func localized(withComment:String) -> String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: withComment)
+    }
 }
--- HelPal/RecommoViewController.swift
+++ HelPal/RecommoViewController.swift
@@ -12,11 +12,33 @@ import UIKit
 //There will be a segmented control on the navi bar, to switch map mode and the list mode
 //Mapbox will do the help
 class RecommonViewController: UIViewController{
+    
+    @IBOutlet weak var listContainer: UIView! //segment 1
+    @IBOutlet weak var mapContainer: UIView! //segment 0
+    
     override func viewDidLoad() {
-        let segment: UISegmentedControl = UISegmentedControl(items: ["RecommonSegementedTitle1".localized, "RecommonSegementedTitle2".localized])
-        segment.sizeToFit()
-        segment.tintColor = UIColor(red:0.99, green:0.00, blue:0.25, alpha:1.00)
+        
+        //Hide list container by default
+        listContainer.isHidden = true;
+        
+        //Adding the segment
+        let segment: UISegmentedControl = UISegmentedControl(items: ["RecommonSegementedTitle1".localized, "RecommonSegementedTitle2".localized]);
+        segment.sizeToFit();
+        segment.tintColor = UIColor(red:0.99, green:0.00, blue:0.25, alpha:1.00);
         segment.selectedSegmentIndex = 0;
-        self.navigationItem.titleView = segment
+        self.navigationItem.titleView = segment;
+        //Swift 3 has changed the way to write addTarget Method
+        segment.addTarget(self, action: #selector(segmentChanged(sender:)), for: .valueChanged);
     }
+    
+    func segmentChanged(sender: UISegmentedControl){
+        if(sender.selectedSegmentIndex == 1){
+            listContainer.isHidden = true;
+            mapContainer.isHidden = false;
+        }else if(sender.selectedSegmentIndex == 0){
+            listContainer.isHidden = false;
+            mapContainer.isHidden = true;
+        }
+    }
+    
 }
