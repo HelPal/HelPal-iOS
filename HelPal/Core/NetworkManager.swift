a2b8a223086e7cf5b25844072e0fef393dfdc320
钟典@Runga <ceo@myaiol.com>
2017-04-09 21:31:25 +0800

    > Introducing PromiseKit

--- HelPal/Core/NetworkManager.swift
+++ HelPal/Core/NetworkManager.swift
@@ -7,9 +7,36 @@
 //
 
 import Foundation
+import Alamofire
+import PromiseKit
 
 final class NetworkManager {
-    private init(){}
+    
+    var networkType: NetworkType!;
+    
+    private init(){
+        if UniversalNetworkingMode == "mock" {
+            self.networkType = .mock;
+        } else if UniversalNetworkingMode == "compare" {
+            self.networkType = .compare;
+        } else{
+            self.networkType = .product;
+        }
+    }
+    
     static let sharedInstance: NetworkManager = NetworkManager();
     
+    func loadJson(url: URLConvertible) -> Promise<NSDictionary> {
+        return Promise { fulfill, reject in
+            Alamofire.request(url).responseJSON { response in
+                fulfill(response.result.value as! NSDictionary);
+            }
+        }
+    }
+}
+
+enum NetworkType {
+    case product;
+    case mock;
+    case compare;
 }
