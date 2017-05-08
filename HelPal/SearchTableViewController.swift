ed80e382e0bff8f224ef0fd67afd3b0bfb264ab4
钟典@Runga <ceo@myaiol.com>
2017-05-09 00:48:24 +0800

    Now the search page can be dismissed

--- HelPal/SearchTableViewController.swift
+++ HelPal/SearchTableViewController.swift
@@ -12,5 +12,25 @@ class SearchTableViewController: UITableViewController {
     override func viewDidLoad() {
         let search = UISearchBar() // Create your search bar
         self.navigationItem.titleView = search;
+        let tapper = UITapGestureRecognizer(target: self, action: #selector(tapOn(tap:)));
+        self.tableView.addGestureRecognizer(tapper);
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath);
+    }
+    
+    func tapOn(tap: UITapGestureRecognizer){
+        let location = tap.location(in: self.tableView);
+        let path = self.tableView.indexPathForRow(at: location);
+        if path == nil {
+            quitSearch();
+        } else{
+            self.tableView(self.tableView, didSelectRowAt: path!);
+        }
+    }
+    
+    func quitSearch(){
+        self.dismiss(animated: true, completion: nil);
     }
 }
