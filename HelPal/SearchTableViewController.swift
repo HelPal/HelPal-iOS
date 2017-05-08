825be9ba7cb455c80d0016f048ba8dee59a14c72
钟典@Runga <ceo@myaiol.com>
2017-05-09 01:01:10 +0800

    First Line of search list will be create

--- HelPal/SearchTableViewController.swift
+++ HelPal/SearchTableViewController.swift
@@ -8,16 +8,48 @@
 
 import UIKit
 
-class SearchTableViewController: UITableViewController {
+class SearchTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    var queryStr: String?;
+    
     override func viewDidLoad() {
-        let search = UISearchBar() // Create your search bar
-        self.navigationItem.titleView = search;
+        let searchBar = UISearchBar() // Create your search bar
+        self.navigationItem.titleView = searchBar;
+        searchBar.delegate = self;
         let tapper = UITapGestureRecognizer(target: self, action: #selector(tapOn(tap:)));
         self.tableView.addGestureRecognizer(tapper);
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath);
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        queryStr = searchText;
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.tableView.reloadData();
+    }
+    
+    //TODO:
+    //make the search page dynamic
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if queryStr != nil && queryStr != ""{
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+        cell.textLabel?.text = "发布关于\"" + queryStr! + "\"的需求或服务";
+        return cell
     }
     
     func tapOn(tap: UITapGestureRecognizer){
